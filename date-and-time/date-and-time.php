961c0dc289b25fc4d2aaa0041cbf27cf33b40c08
nandacjo <nandacojde22@gmail.com>
2021-12-11 16:40:43 +0700

    update

---
+++ date-and-time/date-and-time.php
@@ -0,0 +1,13 @@
+<?php
+
+date_default_timezone_set("Asia/Jakarta");
+
+echo "Hari ini tanggal " . date("Y/M/d") . "\n";
+echo "Hari ini tanggal " . date("Y.m.D") . "\n";
+echo "Hari ini tanggal " . date("Y-m-d") . "\n";
+echo "Hari ini adalah " . date("l") . "\n";
+
+echo "&copy: 2010 - " . date("Y");
+echo "\n";
+
+echo "Jam sekarang adalah : " . date("h:i:s:a");
